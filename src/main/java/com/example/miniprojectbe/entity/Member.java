294c61b88b189ff9872d8b45829d4832ee5998f6
WONSEOKJE <nicholas4744@gmail.com>
2023-02-13 13:30:08 +0900

    feat: 멤버 엔티티 생성

---
+++ src/main/java/com/example/miniprojectbe/entity/Member.java
@@ -0,0 +1,47 @@
+package com.example.miniprojectbe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "MEMBER")
+public class Member {
+
+    @Id
+    @Column(name = "memberId", nullable = false)
+    private String memberId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "birth", nullable = false)
+    private LocalDateTime birth;
+
+    @Column(name = "job", nullable = false)
+    private String job;
+
+    @Column(name = "district", nullable = false)
+    private String district;
+
+    @Column(name = "bank", nullable = false)
+    private String bank;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @OneToMany(mappedBy = "member")
+    private List<Basket> basketList;
+}
