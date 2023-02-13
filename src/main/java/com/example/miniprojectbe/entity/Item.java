d3df361fd950b95551a9657ad016862844bcfc9e
WONSEOKJE <nicholas4744@gmail.com>
2023-02-13 13:31:17 +0900

    feat: 아이템 엔티티 생성 (싱글테이블 전략으로 구현) Deposit와 Loan 엔티티는 Item엔티티를 상속 받음

---
+++ src/main/java/com/example/miniprojectbe/entity/Item.java
@@ -0,0 +1,52 @@
+package com.example.miniprojectbe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Table(name = "ITEM")
+public abstract class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "itemId", nullable = false)
+    private Long itemId;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "bank", nullable = false)
+    private String bank;
+
+    @Column(name = "itemName", nullable = false)
+    private String itemName;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "join", nullable = false)
+    private String join;
+
+    @Column(name = "limit")
+    private int limit;
+
+    @Column(name = "preference")
+    private String preference;
+
+    @Column(name = "target")
+    private String target;
+
+    @OneToMany(mappedBy = "item")
+    private List<Basket> basketList;
+}
