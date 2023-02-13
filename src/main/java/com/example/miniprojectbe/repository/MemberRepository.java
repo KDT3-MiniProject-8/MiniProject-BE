9d895f0390ca302849f335d7f2894be0750fbb78
WONSEOKJE <nicholas4744@gmail.com>
2023-02-13 21:51:23 +0900

    feat: MemberRepository 생성

---
+++ src/main/java/com/example/miniprojectbe/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.miniprojectbe.repository;
+
+import com.example.miniprojectbe.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+    Optional<Member> findByMemberId(String memberId);
+}
