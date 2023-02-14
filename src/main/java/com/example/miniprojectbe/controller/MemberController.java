f8b09192865c58aac49041341d62f17f3f2c8216
WONSEOKJE <nicholas4744@gmail.com>
2023-02-14 18:02:01 +0900

    feat: MemberController 비밀번호 찾기 기능 관련 메서드 추가

--- src/main/java/com/example/miniprojectbe/controller/MemberController.java
+++ src/main/java/com/example/miniprojectbe/controller/MemberController.java
@@ -1,14 +1,14 @@
 package com.example.miniprojectbe.controller;
 
+import com.example.miniprojectbe.dto.MailDTO;
 import com.example.miniprojectbe.dto.MemberRequestDTO;
 import com.example.miniprojectbe.dto.MemberLoginDTO;
 import com.example.miniprojectbe.service.MemberService;
+import com.example.miniprojectbe.service.impl.CreateMailAndUpdatePwServiceImpl;
+import com.example.miniprojectbe.service.impl.SendMailServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.HashMap;
@@ -18,6 +18,8 @@ import java.util.HashMap;
 public class MemberController {
 
     private final MemberService memberService;
+    private final SendMailServiceImpl sendMailServiceImpl;
+    private final CreateMailAndUpdatePwServiceImpl createMailAndUpdatePwServiceImpl;
 
     @PostMapping("/signup")
     public String signup(MemberRequestDTO memberRequestDTO){
@@ -39,4 +41,30 @@ public class MemberController {
         return ResponseEntity.ok(memberService.checkEmailDuplicate(memberId));
     }
 
+    @GetMapping("/findPw")
+    public HashMap<String, String> findPassword(@RequestParam(name = "memberId") String memberId,
+                                                @RequestParam(name = "name") String name) {
+        HashMap<String, String>result= new HashMap<>();
+        String resultCode = memberService.findPassword(memberId, name);
+        result.put("resultCode", resultCode);
+        return result;
+    }
+
+    @PostMapping("/findPw/sendMail")
+    public HashMap<String, String> sendMail(@RequestParam(name = "memberId") String memberId,
+                                            @RequestParam(name = "name") String name) {
+        HashMap<String, String> result = new HashMap<>();
+
+        MailDTO mailDTO = createMailAndUpdatePwServiceImpl.createMailAndChangePassword(memberId, name);
+
+        try {
+            sendMailServiceImpl.sendMail(mailDTO);
+            result.put("resultCode", "success");
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.put("resultCode", "failed");
+        }
+
+        return result;
+    }
 }
