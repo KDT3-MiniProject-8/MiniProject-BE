63262c1fa5c10cf2ecf6c9b26483e30900bfbaa6
dkschdcks <acc33540226@gmail.com>
2023-02-14 20:26:52 +0900

    refactor : loan 생성자 추가            builer 사용시 item(부모클래스)의 객체를 사용하기 위해서

--- src/main/java/com/example/miniprojectbe/entity/Loan.java
+++ src/main/java/com/example/miniprojectbe/entity/Loan.java
@@ -9,11 +9,11 @@ import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import java.math.BigDecimal;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Builder
 @Entity
 @DiscriminatorValue("L")
 public class Loan extends Item{
@@ -26,4 +26,13 @@ public class Loan extends Item{
 
     @Column(name = "delay")
     private String delay;
+
+    @Builder
+    public Loan(Long itemId, String category, String bank, String itemName, String type, String join, int limit, String preference, String target, List<Basket> basketList,
+                   BigDecimal rate, BigDecimal prefRate, String mature) {
+        super(itemId, category, bank, itemName,type,join,limit,preference,target,basketList); // 부모 클래스 생성자 호출
+        this.rate = rate; // 추가된 필드
+        this.prefRate = prefRate; // 추가된 필드
+        this.delay = mature; // 추가된 필드
+    }
 }
