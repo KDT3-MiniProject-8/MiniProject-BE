4ffd7e84aa8074dcf71f9fb9cb801dad376ca2c5
dkschdcks <acc33540226@gmail.com>
2023-02-15 19:21:53 +0900

    feat : 검색어 입력시 조회기능 추가        은행,상품명 속성을 검색하도록 구현

--- src/main/java/com/example/miniprojectbe/repository/DepositRepository.java
+++ src/main/java/com/example/miniprojectbe/repository/DepositRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepositRepository extends JpaRepository<Deposit, Long> {
     Slice<Deposit> findAllByCategory(String category, PageRequest pageRequest);
+
+    List<Deposit> findAllByBankContainingOrItemNameContaining(String content1, String content2);
 
 }
