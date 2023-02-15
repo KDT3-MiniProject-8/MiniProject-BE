420973390538cdcdf93320607d46743131014f2e
maemae22 <maemae2@naver.com>
2023-02-15 20:40:17 +0900

    feat: #27 - BasketRepository에 2개 추가
    - findByBasket : basketId로 찾은 Basket을 Optional로 반환함
    - deleteAllByMember_MemberId : memberId에 해당하는 모든 Basket을 삭제함

--- src/main/java/com/example/miniprojectbe/repository/BasketRepository.java
+++ src/main/java/com/example/miniprojectbe/repository/BasketRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BasketRepository extends JpaRepository<Basket, BasketId> {
 
     @EntityGraph(attributePaths = {"member", "item"})
     List<Basket> findByMember_MemberId(String memberId);
+
+    Optional<Basket> findByBasket(Long basketId);
+
+    void deleteAllByMember_MemberId(String memberId);
 }
