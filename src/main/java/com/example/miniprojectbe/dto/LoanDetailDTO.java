6374aa93ca458bfae6c3fe0627a6c5bc1d4bcb60
suji5262 <suji5262@gmail.com>
2023-02-16 14:25:46 +0900

    feat: 상세조회 Loan DTO 추가

---
+++ src/main/java/com/example/miniprojectbe/dto/LoanDetailDTO.java
@@ -0,0 +1,59 @@
+package com.example.miniprojectbe.dto;
+
+
+import com.example.miniprojectbe.entity.Loan;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoanDetailDTO {
+
+    private Long itemId;
+    private String category;
+    private String bank;
+    private String itemName;
+    private String type;
+    private String join;
+    private Integer limit;
+    private String preference;
+    private String target;
+    private BigDecimal minRate;
+    private BigDecimal maxRate;
+    private String delay;
+
+    public LoanDetailDTO(Long itemId, String category, String bank, String itemName, String type, String join, int limit, String preference, String target, BigDecimal minRate, BigDecimal maxRate, String delay) {
+        this.itemId = itemId;
+        this.category = category;
+        this.bank = bank;
+        this.itemName = itemName;
+        this.type = type;
+        this.join = join;
+        this.limit = limit;
+        this.preference = preference;
+        this.target = target;
+        this.minRate = minRate;
+        this.maxRate = maxRate;
+        this.delay = delay;
+    }
+
+
+    public LoanDetailDTO(Loan loan) {
+        this.itemId = loan.getItemId();
+        this.category = loan.getCategory();
+        this.bank = loan.getBank();
+        this.itemName = loan.getItemName();
+        this.type = loan.getType();
+        this.join = loan.getJoin();
+        this.limit = loan.getLimit();
+        this.preference = loan.getPreference();
+        this.target = loan.getTarget();
+        this.minRate = loan.getMinRate();
+        this.maxRate = loan.getMaxRate();
+        this.delay = loan.getDelay();
+    }
+}
